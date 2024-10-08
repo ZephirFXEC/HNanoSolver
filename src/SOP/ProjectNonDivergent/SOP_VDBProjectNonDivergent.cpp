f1dc0066708b2401bb58589d19b6d236dba7300e
ZephirFXEC <zephirfx.pro@gmail.com>
2024-10-08 13:14:01 +0100

    Placeholder for the Project Non Divergent node.

--- src/SOP/ProjectNonDivergent/SOP_VDBProjectNonDivergent.cpp
+++ src/SOP/ProjectNonDivergent/SOP_VDBProjectNonDivergent.cpp
@@ -2,6 +2,53 @@
 // Created by zphrfx on 03/10/2024.
 //
 
+#include "SOP_VDBProjectNonDivergent.hpp"
+
 #include <UT/UT_DSOVersion.h>
 
-#include "SOP_VDBProjectNonDivergent.hpp"
+
+const char* const SOP_HNanoVDBProjectNonDivergentVerb::theDsFile = R"THEDSFILE(
+{
+    name        parameters
+	parm {
+		name	"velgrid"
+		label	"Velocity Volumes"
+		type	string
+		default	{ "" }
+		parmtag	{ "script_action" "import soputils\nkwargs['geometrytype'] = (hou.geometryType.Primitives,)\nkwargs['inputindex'] = 0\nsoputils.selectGroupParm(kwargs)" }
+		parmtag	{ "script_action_help" "Select geometry from an available viewport.\nShift-click to turn on Select Groups." }
+		parmtag	{ "script_action_icon" "BUTTONS_reselect" }
+	}
+	parm {
+		name "voxelsize"
+		label "Voxel Size"
+        type    float
+        size    1
+        default { "0.5" }
+	}
+}
+)THEDSFILE";
+
+PRM_Template* SOP_HNanoVDBProjectNonDivergent::buildTemplates() {
+	static PRM_TemplateBuilder templ("SOP_VDBProjectNonDivergent.cpp", SOP_HNanoVDBProjectNonDivergentVerb::theDsFile);
+	if (templ.justBuilt()) {
+		templ.setChoiceListPtr("velgrid", &SOP_Node::namedVolumesMenu);
+	}
+	return templ.templates();
+}
+
+void newSopOperator(OP_OperatorTable* table) {
+	table->addOperator(new OP_Operator("hnanoprojectnondivergent", "HNanoProjectNonDivergent",
+	                                   SOP_HNanoVDBProjectNonDivergent::myConstructor,
+	                                   SOP_HNanoVDBProjectNonDivergent::buildTemplates(), 1, 1, nullptr, 0));
+}
+
+
+const SOP_NodeVerb::Register<SOP_HNanoVDBProjectNonDivergentVerb> SOP_HNanoVDBProjectNonDivergentVerb::theVerb;
+
+const SOP_NodeVerb* SOP_HNanoVDBProjectNonDivergent::cookVerb() const {
+	return SOP_HNanoVDBProjectNonDivergentVerb::theVerb.get();
+}
+
+
+void SOP_HNanoVDBProjectNonDivergentVerb::cook(const CookParms& cookparms) const {}
--- src/SOP/ProjectNonDivergent/SOP_VDBProjectNonDivergent.hpp
+++ src/SOP/ProjectNonDivergent/SOP_VDBProjectNonDivergent.hpp
@@ -4,9 +4,69 @@
 
 #pragma once
 
+#include <PRM/PRM_TemplateBuilder.h>
 #include <SOP/SOP_Node.h>
+#include <SOP/SOP_NodeVerb.h>
+#include <nanovdb/NanoVDB.h>
+#include <nanovdb/util/CreateNanoGrid.h>
+#include <nanovdb/util/NanoToOpenVDB.h>
+
+#include "SOP_VDBProjectNonDivergent.proto.h"
+#include "Utils/GridData.hpp"
 
 class SOP_HNanoVDBProjectNonDivergent final : public SOP_Node {
+   public:
+	SOP_HNanoVDBProjectNonDivergent(OP_Network* net, const char* name, OP_Operator* op) : SOP_Node(net, name, op) {
+		mySopFlags.setManagesDataIDs(true);
 
+		// It means we cook at every frame change.
+		OP_Node::flags().setTimeDep(true);
+	}
+
+	~SOP_HNanoVDBProjectNonDivergent() override = default;
+
+	static PRM_Template* buildTemplates();
+
+	static OP_Node* myConstructor(OP_Network* net, const char* name, OP_Operator* op) {
+		return new SOP_HNanoVDBProjectNonDivergent(net, name, op);
+	}
+
+	OP_ERROR cookMySop(OP_Context& context) override { return cookMyselfAsVerb(context); }
+
+	const SOP_NodeVerb* cookVerb() const override;
+
+
+	const char* inputLabel(unsigned idx) const override {
+		switch (idx) {
+			case 0:
+				return "Input Grids";
+			default:
+				return "default";
+		}
+	}
 };
 
+class SOP_HNanoVDBProjectNonDivergentCache final : public SOP_NodeCache {
+   public:
+	SOP_HNanoVDBProjectNonDivergentCache() : SOP_NodeCache() {}
+	~SOP_HNanoVDBProjectNonDivergentCache() override = default;
+};
+
+class SOP_HNanoVDBProjectNonDivergentVerb final : public SOP_NodeVerb {
+   public:
+	SOP_HNanoVDBProjectNonDivergentVerb() = default;
+	~SOP_HNanoVDBProjectNonDivergentVerb() override = default;
+	[[nodiscard]] SOP_NodeParms* allocParms() const override { return new SOP_VDBProjectNonDivergentParms; }
+	[[nodiscard]] SOP_NodeCache* allocCache() const override { return new SOP_HNanoVDBProjectNonDivergentCache(); }
+	[[nodiscard]] UT_StringHolder name() const override { return "HNanoProjectNonDivergent"; }
+
+	SOP_NodeVerb::CookMode cookMode(const SOP_NodeParms* parms) const override { return SOP_NodeVerb::COOK_DUPLICATE; }
+
+	void cook(const SOP_NodeVerb::CookParms& cookparms) const override;
+
+	static const SOP_NodeVerb::Register<SOP_HNanoVDBProjectNonDivergentVerb> theVerb;
+	static const char* const theDsFile;
+};
+
+
+#endif  // SOP_READWRITETEST_HPP
